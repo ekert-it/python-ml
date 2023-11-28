2f2b4ff61999c26a9cebb1431f5e52a13b683a8d
Thomas Ekert <support@ekert.de>
2023-11-28 15:11:44 +0100

    First Steps

--- py/availabe_modules.py
+++
@@ -1,8 +0,0 @@
-'''Alle verfügbaren Module anziegen'''
-import sys
-
-# List all available modules:
-module_list=sys.modules
-print (type(module_list))
-for module in sorted(module_list):
-    print (module)
